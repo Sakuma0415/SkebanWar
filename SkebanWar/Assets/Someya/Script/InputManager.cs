94820207bc285945ee1d284c8cd943fe15169f5e
some0430 <somemasa0430@docomo.ne.jp>
2020-11-20 15:59:51 +0900

    [Input Update]押しっぱなしを追加

--- SkebanWar/Assets/Someya/Script/InputManager.cs
+++ SkebanWar/Assets/Someya/Script/InputManager.cs
@@ -10,6 +10,7 @@ public class InputManager: MonoBehaviour
     {
         foreach(Touch touch in Input.touches)
         {
+            // タブレットを押した時
             if(touch.phase == TouchPhase.Began)
             {
                 // 現在のタッチの座標でレイを作ります
@@ -19,6 +20,27 @@ public class InputManager: MonoBehaviour
                     // ヒットしたらパーティクルを作ります
                     Instantiate(particle, transform.position, transform.rotation);
                 }
+                Debug.Log("押した");
+            }
+            // タブレットを離した時
+            if(touch.phase == TouchPhase.Ended)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if(Physics.Raycast(ray))
+                {
+                    Instantiate(particle, transform.position, transform.rotation);
+                }
+                Debug.Log("離した");
+            }
+            // タブレットを押し続いている時
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray))
+                {
+                    Instantiate(particle, transform.position, transform.rotation);
+                }
+                Debug.Log("押し続いている");
             }
         }
     }
