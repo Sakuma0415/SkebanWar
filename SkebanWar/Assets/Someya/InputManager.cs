1b14e6ec579287bfd5817ca5c243b07505ace8fd
some0430 <somemasa0430@docomo.ne.jp>
2020-11-20 14:08:30 +0900

    [New]入力のマネジャー作成(未検証)

---
+++ SkebanWar/Assets/Someya/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager: MonoBehaviour
+{
+    GameObject particle;
+
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                // 現在のタッチの座標でレイを作ります
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if(Physics.Raycast(ray))
+                {
+                    // ヒットしたらパーティクルを作ります
+                    Instantiate(particle, transform.position, transform.rotation);
+                }
+            }
+        }
+    }
+}
