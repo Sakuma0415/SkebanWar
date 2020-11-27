f78af167b3141c34849552fcdc7450a8912910eb
some0430 <somemasa0430@docomo.ne.jp>
2020-11-27 11:09:38 +0900

    [New]キャラクターの選択(途中)

---
+++ SkebanWar/Assets/Someya/Script/CharacterChoice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterChoice : MonoBehaviour
+{
+    private Button Character1;
+    private Button Character2;
+    private Button Character3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 各ボタンコンポーネントの取得
+        Character1 = GameObject.Find("/Canvas/Character1").GetComponent<Button>();
+        Character2 = GameObject.Find("/Canvas/Character2").GetComponent<Button>();
+        Character3 = GameObject.Find("/Canvas/Character3").GetComponent<Button>();
+    }
+}
