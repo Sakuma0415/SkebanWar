d164917f7e170784b6541e025eb047425d3a0658
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-11-27 17:13:47 +0900

    リザルト実験中

---
+++ SkebanWar/Assets/Iwasaki/Script/Result.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    private Text redText;
+    private Text blueText;
+    void Start()
+    {
+        blueText = GameObject.FindGameObjectWithTag("BlueText").GetComponent<Text>();
+        redText = GameObject.FindGameObjectWithTag("RedText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {        
+        blueText.text = Random.Range(1, 30).ToString();
+        redText.text = Random.Range(1, 30).ToString();
+    }
+}
