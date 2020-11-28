6dde44084be7ac512fd407f31b6ffa1bdba77180
some0430 <somemasa0430@docomo.ne.jp>
2020-11-28 18:46:30 +0900

    [Update]キャラクター選択(仮)

---
+++ SkebanWar/Assets/Someya/Script/CharacterChoice2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterChoice2 : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Character2");
+    }
+}
