0ec36412b390a5b2f7e0f549f9ab9600478dd58d
some0430 <somemasa0430@docomo.ne.jp>
2020-12-04 18:13:13 +0900

    [Update]キャラクター選択(90%完成) のこり10%一部のタイトルシーン,ゲームシーン遷移すること

---
+++ SkebanWar/Assets/Someya/Script/CharacterChoice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterChoice : MonoBehaviour
+{
+    [SerializeField]
+    int select = 0;
+
+    [SerializeField]
+    GameObject[] Character;
+
+    // ボタンを押したら選んだキャラクターを描画
+    public void C1Change()
+    {
+        select = 1;
+    }
+
+    public void C2Change()
+    {
+        select = 2;
+    }
+
+    public void C3Change()
+    {
+        select = 3;
+    }
+
+    // いいえを押したら選択に戻る
+    public void Cancel()
+    {
+        select = 0;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Character.Length; i++)
+        {
+            Character[i].SetActive(i == select); 
+        }
+    }
+}
