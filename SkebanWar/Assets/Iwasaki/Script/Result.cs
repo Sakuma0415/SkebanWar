48e61f41aeb25cd509a21739b8225ada7f067632
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-12-04 16:05:40 +0900

    リザルトpublic化

--- SkebanWar/Assets/Iwasaki/Script/Result.cs
+++ SkebanWar/Assets/Iwasaki/Script/Result.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Result : MonoBehaviour
+public class Result : SingletonMonoBehaviour<Result>
 {
     private Text redPointText;
     private Text bluePointText;
