ab51b3b83fa7410d815c1772be6c348f1519f548
some0430 <somemasa0430@docomo.ne.jp>
2020-12-18 11:48:14 +0900

    キャラクター選択95%完成(5%:ゲームシーン移行のみ)

--- SkebanWar/Assets/Someya/Script/CharacterChoice.cs
+++ SkebanWar/Assets/Someya/Script/CharacterChoice.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CharacterChoice : MonoBehaviour
 {
@@ -15,34 +15,84 @@ public class CharacterChoice : MonoBehaviour
     int choice = 0;
 
     [SerializeField]
-    GameObject[] Decsion;
+    GameObject[] Decision;
 
     // ボタンを押したら選んだキャラクターを描画
     public void C1Change()
     {
+        choice = 0;
         select = 1;
     }
 
     public void C2Change()
     {
+        choice = 0;
         select = 2;
     }
 
     public void C3Change()
     {
+        choice = 0;
         select = 3;
     }
 
-    // いいえを押したら選択に戻る
-    public void Cancel()
+    // いいえを押したらキャラクター選択に戻る
+    public void C1Cancel()
     {
         choice = 1;
-        select = 0;
+        // 1.5秒後にシーン(画像)に移行
+        StartCoroutine(DelayMethod(1.5f, () =>
+        {
+            select = 0;
+        }));
     }
 
-    public void OK()
+    // はいを押したらゲームシーンに移行
+    public void C1OK()
     {
         choice = 2;
+    }
+    
+    public void C2Cancel()
+    {
+        choice = 3;
+        // 1.5秒後にシーン(画像)に移行
+        StartCoroutine(DelayMethod(1.5f, () =>
+        {
+            select = 0;
+        }));
+    }
+
+    public void C2OK()
+    {
+        choice = 4;
+    }
+
+    public void C3Cancel()
+    {
+        choice = 5;
+        // 1.5秒後にシーン(画像)に移行
+        StartCoroutine(DelayMethod(1.5f, () =>
+        {
+            select = 0;
+        }));
+    }
+
+    public void C3OK()
+    {
+        choice = 6;
+    }
+
+    /// <summary>
+    /// 渡された処理を指定時間後に実行する
+    /// </summary>
+    /// <param name="waitTime">遅延時間[ミリ秒]</param>
+    /// <param name="action">実行したい処理</param>
+    /// <returns></returns>
+    private IEnumerator DelayMethod(float waitTime, Action action)
+    {
+        yield return new WaitForSeconds(waitTime);
+        action();
     }
 
     void Update()
@@ -51,9 +101,9 @@ public class CharacterChoice : MonoBehaviour
         {
             Character[i].SetActive(i == select); 
         }
-        for (int j = 0; j < Decsion.Length; j++)
+        for (int j = 0; j < Decision.Length; j++)
         {
-            Decsion[j].SetActive(j == choice);
+            Decision[j].SetActive(j == choice);
         }
     }
 }
