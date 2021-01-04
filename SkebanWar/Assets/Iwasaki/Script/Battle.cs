f45782221ea09b083f4c5aa9f484e74fce18002d
岩崎友哉 <tomoti2525@yahoo.co.jp>
2021-01-04 19:19:36 +0900

    サイコロ前まで完了

--- SkebanWar/Assets/Iwasaki/Script/Battle.cs
+++ SkebanWar/Assets/Iwasaki/Script/Battle.cs
@@ -31,7 +31,7 @@ public class Battle : MonoBehaviour
     //インターバルの時間
     float intervalTime = 0;
 
-    //入力街を持つゲームモードの終了フラグ
+    //入力を持つゲームモードの終了フラグ
     public bool endGameMode = false;
 
     private bool diceBool = true;
@@ -41,35 +41,68 @@ public class Battle : MonoBehaviour
     private GameObject diceButton;
     private bool doOnce = true;
     private int diceNumber;
+
     //キャラクターデータ
     [SerializeField]
     private CharDataBase charData;
     private CharacterData lowerChar;
     private CharacterData upperChar;
-    //上側のキャライメージ
-    private GameObject upperImage;
-    //下側のキャライメージ
-    private GameObject lowerImage;
-    //上側のキャラHP
+
+    //スプライトデータ
+    [SerializeField]
+    private SpriteData spriteData;
+
+    //上、下側のキャライメージ
+    private Image upperImage;
+    private Image lowerImage;
+
+    //上、下側のキャラHP
     [SerializeField]
     private Text upperText;
-    //下側のキャラHP
     [SerializeField]
     private Text lowerText;
+
+    //喧嘩上等 時の左右のキャラクターイメージ定義
+    private Image kenkaImage_Left;
+    private Image kenkaImage_Right;
+
+    //喧嘩上等 時の左右のプレートイメージ定義
+    private Image plateLeft;
+    private Image plateRight;
+
     [SerializeField]
     private CanvasGroup fadeCanvas;
+
+    //アニメーター定義
     private Animator anim_Icon;
     private Animator anim_Plate;
+    private Animator anim_Text;
+
+    //先攻後攻決め(true=1P,false=2P)
+    private bool witchAttackBool = false;
 
     void Start()
     {
+        Instance = this;
+        fadeCanvas.alpha = 0;
+
+        //アニメーター初期化
         anim_Icon = GameObject.FindGameObjectWithTag("IconAnim").GetComponent<Animator>();
         anim_Plate = GameObject.FindGameObjectWithTag("PlateAnim").GetComponent<Animator>();
+        anim_Text = GameObject.FindGameObjectWithTag("TextAnim").GetComponent<Animator>();
 
-        fadeCanvas.alpha = 0;
-        Instance = this;
-        upperImage = GameObject.FindGameObjectWithTag("UpperImage");
-        lowerImage = GameObject.FindGameObjectWithTag("LowerImage");
+        //キャラクターアイコン初期化
+        upperImage = GameObject.FindGameObjectWithTag("UpperImage").GetComponent<Image>();
+        lowerImage = GameObject.FindGameObjectWithTag("LowerImage").GetComponent<Image>();
+
+        //喧嘩上等 時の左右のキャラクターイメージ初期化
+        kenkaImage_Left = GameObject.FindGameObjectWithTag("PlateImageLeft").GetComponent<Image>();
+        kenkaImage_Right = GameObject.FindGameObjectWithTag("PlateImageRight").GetComponent<Image>();
+
+        //喧嘩上等 時の左右のプレートイメージ初期化
+        plateLeft = GameObject.FindGameObjectWithTag("LeftPlate").GetComponent<Image>();
+        plateRight = GameObject.FindGameObjectWithTag("RightPlate").GetComponent<Image>();
+
         //キャラクター情報追加
         upperChar = charData.characterDatas[0];
         lowerChar = charData.characterDatas[8];
@@ -79,11 +112,28 @@ public class Battle : MonoBehaviour
         lowerChar.HP = 7;
 
         //ImageとHPを表示
-        upperImage.GetComponent<Image>().sprite = upperChar.Image;
+        upperImage.sprite = upperChar.Image;
         upperText.text = upperChar.HP.ToString();
-        lowerImage.GetComponent<Image>().sprite = lowerChar.Image;
+        lowerImage.sprite = lowerChar.Image;
         lowerText.text = lowerChar.HP.ToString();
-        //サイコロ時消すか消さないか
+
+        //先攻後攻で画像を分ける
+        if (witchAttackBool)
+        {
+            plateLeft.sprite = spriteData.Sprites[2];
+            plateRight.sprite = spriteData.Sprites[1];
+            kenkaImage_Left.sprite = lowerChar.IconImage;
+            kenkaImage_Right.sprite = upperChar.IconImage;
+        }
+        else
+        {
+            plateLeft.sprite = spriteData.Sprites[0];
+            plateRight.sprite = spriteData.Sprites[3];
+            kenkaImage_Left.sprite = upperChar.IconImage;
+            kenkaImage_Right.sprite = lowerChar.IconImage;
+        }
+
+        //サイコロ時キャラ画像を消すか消さないか
         //upperImage.gameObject.SetActive(false);
         //lowerImage.gameObject.SetActive(false);        
     }
@@ -142,11 +192,26 @@ public class Battle : MonoBehaviour
             fadeCanvas.alpha = 1f * (time / fadeTime);
             yield return null;
         }
+
         anim_Plate.SetTrigger("Plates");
         yield return new WaitForSeconds(anim_Plate.GetCurrentAnimatorStateInfo(0).length);
+
         anim_Icon.SetTrigger("Icons");
         yield return new WaitForSeconds(anim_Icon.GetCurrentAnimatorStateInfo(0).length);
-        ChagngeGameMode(BattleProcess.FirstDice, 1f);
+
+        anim_Text.SetTrigger("KenkaTexts");        
+        yield return new WaitForSeconds(anim_Text.GetCurrentAnimatorStateInfo(0).length + 1.2f);
+
+        while (kenkaImage_Right.color.r < 1)
+        {
+            time += Time.deltaTime;
+            kenkaImage_Right.color = new Color(0.4f * (time / fadeTime), 0.4f * (time / fadeTime), 0.4f * (time / fadeTime));
+            yield return null;
+        }
+        yield return new WaitForSeconds(2.0f);
+
+        //ChagngeGameMode(BattleProcess.FirstDice, 1f);
+
         yield break;
     }
 
@@ -234,14 +299,15 @@ public class Battle : MonoBehaviour
 
             switch (nowProcess)
             {
-                case BattleProcess.FirstDice:                    
+                case BattleProcess.FirstDice:
+                    doOnce = true;
                     break;
                 case BattleProcess.FirstAttack:
                     doOnce = true;
                     diceBool = true;
                     diceText.gameObject.SetActive(false);
                     diceButton.gameObject.SetActive(false);
-                    //サイコロ時消すか消さないか
+                    //サイコロ時キャラ画像を消すか消さないか
                     //upperImage.gameObject.SetActive(true);
                     //lowerImage.gameObject.SetActive(true);
                     break;
