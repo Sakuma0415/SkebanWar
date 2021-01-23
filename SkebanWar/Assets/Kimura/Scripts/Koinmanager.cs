2428335357a224495c70b4cc379ac78f4d258e8c
Kikura142 <50509211+Kikura142@users.noreply.github.com>
2021-01-23 11:02:55 +0900

    [update]先攻後攻のランダム表示の追加

--- SkebanWar/Assets/Kimura/Scripts/Koinmanager.cs
+++ SkebanWar/Assets/Kimura/Scripts/Koinmanager.cs
@@ -8,41 +8,46 @@ public class Koinmanager : MonoBehaviour
     public Image sampleImage;
     public Sprite Koin1;
     public Sprite Koin2;
-    public Text text;
-    public Text text2;
-    public Text text3;
+    public Text _text;
+    public GameObject text;
+
+    bool test = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetMouseButtonDown(0))
+        if (test == true)
         {
-            transform.position = new Vector3(0, 1600, 0);
-
-            Invoke("Koin_", 2.0f);
+            if (Input.GetMouseButtonDown(0))
+            {
+                transform.position = new Vector3(0, 1600, 0);
+                Invoke("Koin_", 2.0f);
+                test = false;
+            }
         }
     }
     void Koin_()
     {
         transform.position = new Vector3(0, 0, 0);
         int a = Random.Range(0, 2);
+        GameObject obj = GameObject.Find("Text1");
+        Destroy(obj);
         if (a == 0)
         {
-            text.text = "1Pが後攻です";
             sampleImage.sprite = Koin1;
+            _text.text = "1Pが<size=100>後攻</size>です";
         }
         else
         {
-            text.text = "1Pが先攻です";
             sampleImage.sprite = Koin2;
+            _text.text = "1Pが<size=100>先攻</size>です";
         }
-
+        text.SetActive(true);
     }
 }
